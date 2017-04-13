da432f2970f474f1bec3acc246de8a10f6a6b364
Kyle Pastor <kyleanthonypastor@gmail.com>
2017-04-13 14:07:13 -0400

    Added settings activity
    Added for gamplay and accessibility options

---
+++ AppSource/app/src/main/java/com/kylepastor/justcards/MainActivity.java
@@ -0,0 +1,46 @@
+package com.kylepastor.justcards;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.action_bar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // action with ID action_refresh was selected
+            case R.id.action_refresh:
+                Toast.makeText(this, "Refresh selected", Toast.LENGTH_SHORT)
+                        .show();
+                break;
+            // action with ID action_settings was selected
+            case R.id.action_settings:
+                Toast.makeText(this, "The government is poo.", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(this, SettingsActivity.class);
+
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
+
+        return true;
+    }
+}
